147aa936bbfa986df99c3c0cd8b1474e1c52cc11
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-02-16 19:20:18 +0100

    Metiendo el header por componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./Header"
+
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold text-red-500 underline text-center my-9'>Funcamos?</h1>
+      <Header/>
     </>
   )
 }
