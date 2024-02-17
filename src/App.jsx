404755cd75bee8ecf5ee1b067097272b68d1f318
printMelen <alvaro.redondofgb@gmail.com>
2024-02-17 02:16:41 +0100

    Header y Formulario acabado con diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./Header"
+import Login from "./Login"
+import "./App.css"
 
+import Formulario from "./Login"
 function App() {
   return (
     <>
       <Header/>
+      <Login/>
     </>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-
+        <h1 className='text-white text-center my-7 text-2xl uppercase'>Chat de daw-2 de react</h1>
     </header>
   )
 }
